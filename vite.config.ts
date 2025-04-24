259db25d49da67b7d4fd8a1735c6e7a9ff60a11f
xxziiko <xxziiko@gmail.com>
2025-04-25 02:00:38 +0900

    styles: mixins, variables 초기세팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,24 @@
+import path from 'node:path';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+const currentDir = dirname(fileURLToPath(import.meta.url));
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+				@use "${join(currentDir, './src/shared/styles/_variables')}" as *;
+				@use "${join(currentDir, './src/shared/styles/_mixins')}" as *;
+			`,
+        includePaths: [path.join(__dirname, 'src', 'shared', 'styles')],
+      },
+    },
+  },
 });
