22155130db4a7b8d1d405df785ec5f92a2b3266a
xxziiko <xxziiko@gmail.com>
2025-04-25 04:46:15 +0900

    chore : Loader 파일 /shared/loading 폴더로 이동

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,5 +1,5 @@
-export { default as Loader } from './Loader';
 export { default as ErrorFallback } from './error/ErrorFallback';
+export { default as Loader } from './loading/Loader';
 export { default as ContentSlides } from './slide/ContentSlides';
 export { default as NavigationTab } from './navigation-tab/NavigationTab';
 export { default as Carousel } from './carousel/Carousel';
